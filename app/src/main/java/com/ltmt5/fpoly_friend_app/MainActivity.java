dc1ca7c7dfa7bbd21c74ca1ae23d190284898d03
Minh Trinh <91252294+minhtc02@users.noreply.github.com>
2022-10-10 08:46:12 +0700

    first commit

---
+++ app/src/main/java/com/ltmt5/fpoly_friend_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ltmt5.fpoly_friend_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
