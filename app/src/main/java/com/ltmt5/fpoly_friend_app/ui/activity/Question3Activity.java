58b14d24912aeaddd201efad018c2c15830d329a
Minh Trinh <91252294+minhtc02@users.noreply.github.com>
2022-11-21 23:14:23 +0700

    update layout 2

--- app/src/main/java/com/ltmt5/fpoly_friend_app/ui/activity/Question3Activity.java
+++ app/src/main/java/com/ltmt5/fpoly_friend_app/ui/activity/Question3Activity.java
@@ -1,20 +1,78 @@
 package com.ltmt5.fpoly_friend_app.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 import com.ltmt5.fpoly_friend_app.R;
-import com.ltmt5.fpoly_friend_app.databinding.ActivityQuestion2Binding;
 import com.ltmt5.fpoly_friend_app.databinding.ActivityQuestion3Binding;
+import com.ltmt5.fpoly_friend_app.help.PublicData;
 
 public class Question3Activity extends AppCompatActivity {
-
     ActivityQuestion3Binding binding;
+    String mGender = "none";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityQuestion3Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.btnNext.setOnClickListener(v -> {
+            if (!mGender.equals("none")) {
+                PublicData.gender = mGender;
+                startActivity(new Intent(this, Question4Activity.class));
+            }
+        });
+        binding.btnMale.setOnClickListener(v -> {
+            mGender = "Male";
+
+            binding.btnMale.setCardBackgroundColor(ContextCompat.getColor(this, R.color.prime_1));
+            binding.tvMale.setTextColor(ContextCompat.getColor(this, R.color.white));
+
+            binding.btnFemale.setCardBackgroundColor(ContextCompat.getColor(this, R.color.transparent));
+            binding.tvFemale.setTextColor(ContextCompat.getColor(this, R.color.prime_1));
+
+            binding.btnOther.setCardBackgroundColor(ContextCompat.getColor(this, R.color.transparent));
+            binding.tvOther.setTextColor(ContextCompat.getColor(this, R.color.prime_1));
+
+            binding.btnNext.setCardBackgroundColor(ContextCompat.getColor(this, R.color.prime_1));
+            binding.tvNext.setTextColor(ContextCompat.getColor(this, R.color.white));
+
+        });
+
+        binding.btnFemale.setOnClickListener(v -> {
+            mGender = "Female";
+
+            binding.btnFemale.setCardBackgroundColor(ContextCompat.getColor(this, R.color.prime_1));
+            binding.tvFemale.setTextColor(ContextCompat.getColor(this, R.color.white));
+
+            binding.btnMale.setCardBackgroundColor(ContextCompat.getColor(this, R.color.transparent));
+            binding.tvMale.setTextColor(ContextCompat.getColor(this, R.color.prime_1));
+
+            binding.btnOther.setCardBackgroundColor(ContextCompat.getColor(this, R.color.transparent));
+            binding.tvOther.setTextColor(ContextCompat.getColor(this, R.color.prime_1));
+
+            binding.btnNext.setCardBackgroundColor(ContextCompat.getColor(this, R.color.prime_1));
+            binding.tvNext.setTextColor(ContextCompat.getColor(this, R.color.white));
+        });
+
+        binding.btnOther.setOnClickListener(v -> {
+            mGender = "Other";
+
+            binding.btnOther.setCardBackgroundColor(ContextCompat.getColor(this, R.color.prime_1));
+            binding.tvOther.setTextColor(ContextCompat.getColor(this, R.color.white));
+
+            binding.btnFemale.setCardBackgroundColor(ContextCompat.getColor(this, R.color.transparent));
+            binding.tvMale.setTextColor(ContextCompat.getColor(this, R.color.prime_1));
+
+            binding.btnMale.setCardBackgroundColor(ContextCompat.getColor(this, R.color.transparent));
+            binding.tvMale.setTextColor(ContextCompat.getColor(this, R.color.prime_1));
+
+            binding.btnNext.setCardBackgroundColor(ContextCompat.getColor(this, R.color.prime_1));
+            binding.tvNext.setTextColor(ContextCompat.getColor(this, R.color.white));
+        });
     }
+
 }
--- app/src/main/java/com/ltmt5/fpoly_friend_app/ui/activity/Question5Activity.java
+++ app/src/main/java/com/ltmt5/fpoly_friend_app/ui/activity/Question5Activity.java
@@ -1,5 +1,6 @@
 package com.ltmt5.fpoly_friend_app.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.ltmt5.fpoly_friend_app.databinding.ActivityQuestion5Binding;
 
 public class Question5Activity extends AppCompatActivity {
-
     ActivityQuestion5Binding binding;
 
     @Override
@@ -15,5 +15,19 @@ public class Question5Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityQuestion5Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.btnNext.setOnClickListener(v -> {
+            if (validate()) {
+                startActivity(new Intent(this, Question6Activity.class));
+            }
+        });
+    }
+
+    boolean validate() {
+        boolean isDone = true;
+//        if (binding.ed1.getText().toString().trim().equals("")) {
+//            Toast.makeText(this, "Ngày sinh không được để trống", Toast.LENGTH_SHORT).show();
+//            isDone = false;
+//        }
+        return isDone;
     }
 }
