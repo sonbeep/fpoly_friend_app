e5e579004c250c20822a097c718cd634661c5f34
KingWind2906 <94579182+KingWind2906@users.noreply.github.com>
2022-12-25 12:35:35 +0700

    upcode and fix user fragment

---
+++ app/src/main/java/com/ltmt5/fpoly_friend_app/model/Profile.java
@@ -0,0 +1,99 @@
+package com.ltmt5.fpoly_friend_app.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Profile implements Parcelable {
+    public static final Creator<Profile> CREATOR = new Creator<Profile>() {
+        @Override
+        public Profile createFromParcel(Parcel in) {
+            return new Profile(in);
+        }
+
+        @Override
+        public Profile[] newArray(int size) {
+            return new Profile[size];
+        }
+    };
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("url")
+    @Expose
+    private String imageUrl;
+    @SerializedName("age")
+    @Expose
+    private Integer age;
+    @SerializedName("location")
+    private String location;
+
+    public Profile(String name, String imageUrl, String location) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.location = location;
+    }
+
+    protected Profile(Parcel in) {
+        name = in.readString();
+        imageUrl = in.readString();
+        if (in.readByte() == 0) {
+            age = null;
+        } else {
+            age = in.readInt();
+        }
+        location = in.readString();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(imageUrl);
+        if (age == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeInt(age);
+        }
+        dest.writeString(location);
+    }
+}
