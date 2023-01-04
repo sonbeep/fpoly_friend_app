12c4225d8aaf6a7c59baddff296f2d12a144ea16
duongld2002 <duongld2002@gmail.com>
2023-01-04 14:02:56 +0700

    On Board Update

--- app/src/main/java/com/ltmt5/fpoly_friend_app/ui/activity/LD_OnBoardActivity.java
+++ app/src/main/java/com/ltmt5/fpoly_friend_app/ui/activity/LD_OnBoardActivity.java
@@ -2,15 +2,28 @@ package com.ltmt5.fpoly_friend_app.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.ltmt5.fpoly_friend_app.App;
 import com.ltmt5.fpoly_friend_app.R;
+import com.ltmt5.fpoly_friend_app.databinding.ActivityLdOnBoardBinding;
 
 public class LD_OnBoardActivity extends AppCompatActivity {
+    ActivityLdOnBoardBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ld_on_board);
+        binding = ActivityLdOnBoardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btnNext.setOnClickListener(v -> {
+            App.sharePref.setFirstTimeLaunch(false);
+            startActivity(new Intent(this, LogInActivity.class));
+        });
+        binding.btnSignIn.setOnClickListener(v -> {
+            App.sharePref.setFirstTimeLaunch(false);
+            startActivity(new Intent(this, SignInActivity.class));
+        });
     }
 }
