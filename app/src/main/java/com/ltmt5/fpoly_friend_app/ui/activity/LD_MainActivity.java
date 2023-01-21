617b7349f7acd6228fcf4031ff5a751dc19c78dd
duongld2002 <duongld2002@gmail.com>
2023-01-21 11:34:11 +0700

    LD MainActivity

---
+++ app/src/main/java/com/ltmt5/fpoly_friend_app/ui/activity/LD_MainActivity.java
@@ -0,0 +1,16 @@
+package com.ltmt5.fpoly_friend_app.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ltmt5.fpoly_friend_app.R;
+
+public class LD_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ld_main);
+    }
+}
